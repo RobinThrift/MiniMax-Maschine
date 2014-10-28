e397d5e21bab2b34bbb8e34b89ecd0d4207e6f00
Robin Thrift <robin@we-two-design.com>
2014-10-28 22:49:03 +0100

    fonts gewechselt

--- build.sh
+++ build.sh
@@ -1,1 +1,1 @@
-pandoc -f latex --toc ./docs/pflichtenheft.tex -o out/pflichtenheft.pdf
+pandoc -f latex --toc ./docs/pflichtenheft.tex -o out/pflichtenheft.pdf --latex-engine=xelatex --variable mainfont=Helvetica\ Neue --variable monofont=Menlo\ Regular
